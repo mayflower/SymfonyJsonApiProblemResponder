35ee4739b0228876f0c6d0276367201ba34ae29e
Tobias Mogdans <tobias.mogdans@mayflower.de>
2022-05-09 13:48:56 +0200

    OM-283 get rid of logger

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,17 +5,12 @@ declare(strict_types=1);
 namespace Customerce\JsonApiProblemResponder\EventListener;
 
 use Customerce\JsonApiProblemResponder\Exceptions\BaseException;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Throwable;
 
 class ExceptionListener
 {
-    public function __construct(private LoggerInterface $logger)
-    {
-    }
-
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
@@ -25,8 +20,6 @@ class ExceptionListener
         if ($exception instanceof BaseException) {
             $data = $exception->toArray();
         }
-
-        $this->logException($exception);
 
         $response = new JsonResponse($data, $data['status'], ['Content-Type' => 'application/problem+json']);
 
@@ -41,17 +34,5 @@ class ExceptionListener
             'detail' => $exception->getMessage(),
             'type' => 'https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html',
         ];
-    }
-
-    protected function logException(Throwable $exception): void
-    {
-        $message = sprintf(
-            'An error occured: %s with code: %s \n %s',
-            $exception->getMessage(),
-            $exception->getCode(),
-            $exception->getTraceAsString(),
-        );
-
-        $this->logger->error($message);
     }
 }
